ee745bc6fb7e5399843170394ba1b359a969c908
ishmeal <theoleehusky@gmail.com>
2024-11-11 15:37:57 +0000

    first iteration

--- src/naive.cpp
+++ src/naive.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <string_view>
@@ -26,9 +27,9 @@ int wavefront(std::string_view a, std::string_view b, int x, int o, int e) {
 	std::vector dp(a.size() + 1, std::vector(b.size() + 1, std::array<int,3>({100'000'000, 100'000'000, 100'000'000}))); // M, I, D
 
 	dp[0][0] = {0, o, o};
-	std::vector<std::vector<std::pair<int,int>>> q(1, {{0,0}});
+	std::vector<std::vector<std::pair<size_t,size_t>>> q(1, {{0,0}});
 	for (int s = 0; ; s++) {
-		for (int k = 0; k < q[s].size(); k++) {
+		for (size_t k = 0; k < q[s].size(); k++) {
 			auto [i, j] = q[s][k];
 			if (i == a.size() && j == b.size()) return s;
 
@@ -39,17 +40,21 @@ int wavefront(std::string_view a, std::string_view b, int x, int o, int e) {
 					while (q.size() <= cost) q.push_back({});
 					q[cost].push_back({i+1, j+1});
 				}
-			} else if (i < a.size()) {
-				int cost = dp[i][j][0] + o + e;
-				if (cost < dp[i+1][j][0]) {
-					dp[i+1][j][0] = cost;
+			}
+			if (i < a.size()) {
+				int cost = std::min(dp[i][j][0] + o + e, dp[i][j][2] + e);
+				if (cost < dp[i+1][j][2]) {
+					dp[i+1][j][2] = cost;
+					dp[i+1][j][0] = std::min(dp[i+1][j][0], cost);
 					while (q.size() <= cost) q.push_back({});
 					q[cost].push_back({i+1, j});
 				}
-			} else if (j < b.size()) {
-				int cost = dp[i][j][0] + o + e;
-				if (cost < dp[i][j+1][0]) {
-					dp[i][j+1][0] = cost;
+			} 
+			if (j < b.size()) {
+				int cost = std::min(dp[i][j][0] + o + e, dp[i][j][1] + e);
+				if (cost < dp[i][j+1][1]) {
+					dp[i][j+1][1] = cost;
+					dp[i][j+1][0] = std::min(dp[i][j+1][0], cost);
 					while (q.size() <= cost) q.push_back({});
 					q[cost].push_back({i, j+1});
 				}
@@ -62,7 +67,20 @@ int wavefront(std::string_view a, std::string_view b, int x, int o, int e) {
 int main() {
 	/*std::string a("GATACA"), b("GAGATA");*/
 
-	std::string a(1000, 'B'), b(1000, 'B');
+	const int N = 2000;
+	std::string a (N, ' '), b(N, ' ');
+	for (int i = 0; i < N; i++) {
+		a[i] = b[i] = "ACGT"[rand() % 4];
+		if (rand() % 20) continue;
+		while (a[i] == b[i]) a[i] =  "ACGT"[rand() % 4];
+	}
+
+	auto t = std::chrono::system_clock::now();
 	std::cout << naive(a,b, 4, 6, 2) << std::endl;
+	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - t).count() << std::endl;
+
+	t = std::chrono::system_clock::now();
 	std::cout << wavefront(a,b, 4, 6, 2) << std::endl;
+	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - t).count() << std::endl;
+
 }
