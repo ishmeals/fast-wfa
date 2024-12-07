aa932504db9fc0beecb2460c416b1e57063a349b
Noah <noah@ontheplanet.net>
2024-12-06 23:52:21 -0500

    fix comments

--- src/wfa_tool.cpp
+++ src/wfa_tool.cpp
@@ -6,6 +6,8 @@
 #include "fmt/chrono.h"
 #include <chrono>
 
+//Naive is disabled for performance reasons
+
 int main() {
     auto sequences = wfa::modify_sequences(100, 100000, 0.2);
     /*int32_t i = 0;
@@ -54,27 +56,4 @@ int main() {
     end = std::chrono::system_clock::now();
     fmt::println("Naive: {:%T}", end - start);*/
 
-   /* start = std::chrono::system_clock::now();
-    for (const auto& pair : sequences) {
-        const std::string& a = pair.first;
-        const std::string& b = pair.second;
-        wfa::wavefront(a, b, 4, 6, 2);
-    }
-    end = std::chrono::system_clock::now();
-    fmt::println("Wavefront DP: {:%T}", end - start);*/
-
-    /*wfa::wavefront_arena_t arena1;
-    std::string a = "TCATGCAGGCCCGGAGGAAAATTATCCCGGGACGATCTCTAGTTATTGCAGTGGCCAGCGTTGGGGACGGTCGTTTTCGCTCCCAGAACGCCAACCGTGT";
-    std::string b = "TCATGCAGGCCCGAAGGAAAATTAGTCCGGGATGATTTCTTGCTATTTCAGTGGCCAGCATTGGGGCATATCGCTTCAGTTCCCATAAGGTCGACCGTGA";
-
-    auto score = wfa::wavefront_simd(a, b, 4, 6, 2, arena1);
-    auto score2 = wfa::naive(a, b, 4, 6, 2);
-    auto score3 = wfa::wavefront(a, b, 4, 6, 2, arena1);
-
-    fmt::println("{} {} {}", score, score2, score3);*/
-   /* wfa::wavefront_arena_t arena1;
-    std::string a = "AGGATGCTCG";
-    std::string b = "ACCATACTCG";
-    wfa::wavefront(a, b, 4, 6, 2, arena1);*/
-    /* fmt::println("{} {} {}", score, score2, score3);*/
 }
